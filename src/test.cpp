630ba2506e9ed668a4d9dbd6f6c5fc3080be2f4b
Daniel Zhao <dzhao01186@gmail.com>
2022-01-31 19:22:13 -0600

    added tests for control group

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <thread>
+#include <memory>
 #include <string>
 #include "controlGroup.h"
 #include "group.h"
@@ -9,6 +10,7 @@
 using std::to_string;
 using MotorLib::Motor;
 using MotorLib::ControlGroup;
+using MotorLib::Group;
 using namespace std::this_thread;
 using namespace std::chrono;
 
@@ -17,26 +19,43 @@ void printControlGroup(const ControlGroup &obj);
 int main() {
 
     ctre::phoenix::platform::can::SetCANInterface("can0");
-    Motor testMotor0 {1};
     std::cout << "Starting motor" << std::endl;
     ctre::phoenix::unmanaged::FeedEnable(100000);
-    
-    testMotor0.reset();
-    sleep_for(seconds(5));
-    printControlGroup(testMotor0);
-    testMotor0.reset();
-    printControlGroup(testMotor0);
-    testMotor0.setZeroPowerBehavior(MotorLib::ZeroPowerBehavior::COAST);
-    sleep_for(seconds(5));
-    printControlGroup(testMotor0);
-    testMotor0.reset();
-    printControlGroup(testMotor0);
 
-    testMotor0.setPower(0.05);
+    std::shared_ptr<Motor> testMotor1 = std::make_shared<Motor>(1);
+    std::shared_ptr<Motor> testMotor2 = std::make_shared<Motor>(2);
+    Group testGroup1 {"testGroup1"};
+
+    testGroup1.addControlGroup(testMotor1);
+    testGroup1.addControlGroup(testMotor2);
+    testGroup1.reset();
+    printControlGroup(testGroup1);
+    testGroup1.setPrimaryEncoder(testMotor1);
+    testGroup1.setPower(0.1);
+    sleep_for(seconds(5));
+    testGroup1.setZeroPowerBehavior(MotorLib::ZeroPowerBehavior::COAST);
+    testGroup1.setPower(0.1);
+    sleep_for(seconds(5));
+    
+    testGroup1.removeControlGroup(testGroup1.getControlGroup("2"));
+    testGroup1.reset();
+    printControlGroup(testGroup1);
+    testGroup1.setPrimaryEncoder(testMotor1);
+    testGroup1.setPower(0.1);
+    sleep_for(seconds(5));
+    testGroup1.setZeroPowerBehavior(MotorLib::ZeroPowerBehavior::COAST);
+    testGroup1.setPower(0.1);
+    sleep_for(seconds(5));
+
+    testGroup1.clearGroup();
+    printControlGroup(testGroup1);
+
+
+    
 };
 
 
 void printControlGroup(const ControlGroup &obj){
-    printf("Motor Name:  %s\n", obj.getName().c_str());
+    printf("Motor Name:  %s\n", obj.getMembers().c_str());
     printf("Encoder Value:  %s\n", to_string(obj.getEncoder()).c_str());
 }
