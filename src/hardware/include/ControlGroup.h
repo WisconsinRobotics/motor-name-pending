f7ddae0b4f4fa05b57a088ce3214e2faacd703a9
Ben Nowotny <bennowotny65535@gmail.com>
2022-12-01 14:13:20 -0600

    Adding TODO

--- src/hardware/include/ControlGroup.h
+++ src/hardware/include/ControlGroup.h
@@ -14,7 +14,8 @@ public:
     virtual void setZeroPowerBehavior(ZeroPowerBehavior inputBehavior) = 0;
     virtual void setReversal(bool inverted) = 0;
     [[nodiscard]] virtual auto getName() const -> std::string = 0;
-    [[nodiscard]] virtual auto getMembers() const -> std::string = 0;
+    [[nodiscard]] virtual auto getMembers() const
+        -> std::string = 0; // TODO: Is this necessary?
     virtual void resetSettings() const = 0;
 
     ControlGroup() = default;
