1563843f826b20a5f9fbfd819c150c42ba77fe10
Ben Nowotny <bennowotny65535@gmail.com>
2022-12-07 20:02:40 -0600

    Adding new ROS implementation

---
+++ src/ros-handler/include/ControlGroupRosHandler.h
@@ -0,0 +1,18 @@
+#ifndef __CONTROL_GROUP_ROS_HANDLER__
+#define __CONTROL_GROUP_ROS_HANDLER__
+
+#include "ControlGroup.h"
+#include <memory>
+
+namespace RosHandler{
+
+using Hardware::ControlGroup;
+
+class ControlGroupRosHandler {
+    public:
+    private:
+    std::shared_ptr<ControlGroup> controlGroup;
+};
+}
+
+#endif
---
+++ src/ros-handler/include/TimedRosAction.h
@@ -0,0 +1,13 @@
+#ifndef __TIMED_ROS_ACTION__
+#define __TIMED_ROS_ACTION__
+
+#include "ros/node_handle.h"
+#include "ros/time.h"
+namespace RosHandler{
+    class TimedRosAction{
+        public:
+        TimedRosAction(const ros::Time& time, const ros::NodeHandle& node, )
+    };
+}
+
+#endif
