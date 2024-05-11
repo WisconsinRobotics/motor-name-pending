0abbcfa6fca48f7f55428140fa2201e2eb093a55
wiscrobo <wiscrobo@wrover.local>
2024-05-10 19:52:56 -0500

    Colin's changes from basestation

--- src/ros-handler/src/HeadingTimedAction.cpp
+++ src/ros-handler/src/HeadingTimedAction.cpp
@@ -12,8 +12,11 @@ HeadingTimedAction::HeadingTimedAction(ros::NodeHandle &node, const std::shared_
                            ros::Rate{HEADING_PUBLISHER_RATE_HZ}),
       publisher{node.advertise<std_msgs::Float64>("/heading", HEADING_PUBLISHER_BUFFER_SIZE)} {}
 
+
 void HeadingTimedAction::onTimerEvent(const ros::TimerEvent &event) {
     std_msgs::Float64 msg;
+    
+    auto compass = pigeon->GetAbsoluteCompassHeading();
     auto yaw = pigeon->getYaw();
     if (yaw.has_value()) {
         msg.data = *yaw;
